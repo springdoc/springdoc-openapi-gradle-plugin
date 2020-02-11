5d316393ca35015e2f02e27c0677d95e28256730
Ramesh KP <ramesh.ponnuswamy@wipro.com>
2020-02-11 07:38:08 +0000

    Feat: Add readme file and an initial working plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
         name = "Spring Repositories"
         url = uri("https://repo.spring.io/libs-release/")
     }
+    gradlePluginPortal()
 }
 
 publishing {
@@ -31,6 +32,8 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation(group = "khttp", name = "khttp", version = "1.0.0")
     implementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
+    implementation(group = "com.jayway.awaitility", name = "awaitility", version = "1.7.0")
+    implementation(files("/home/ramesh/gradle-processes/build/libs/gradle-processes-0.5.0.jar"))
 }
 
 gradlePlugin {
