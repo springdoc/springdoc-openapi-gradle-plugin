519dedbe215d73f21e3a9ecd989505c49846a333
bnasslahsen <badr.nasslashen@gmail.com>
2020-06-10 21:05:29 +0200

    project update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,74 +1,78 @@
 plugins {
-    `java-gradle-plugin`
-    id("com.gradle.plugin-publish") version "0.11.0"
-    id("org.sonarqube") version "2.8"
-    kotlin("jvm") version "1.3.70"
-    `maven-publish`
+	`java-gradle-plugin`
+	id("com.gradle.plugin-publish") version "0.11.0"
+	id("org.sonarqube") version "2.8"
+	kotlin("jvm") version "1.3.70"
+	`maven-publish`
 }
 
 group = "org.springdoc"
 version = "1.3.1-SNAPSHOT"
 
 sonarqube {
-    properties {
-        property("sonar.projectKey", "springdoc_springdoc-openapi-gradle-plugin")
-    }
+	properties {
+		property("sonar.projectKey", "springdoc_springdoc-openapi-gradle-plugin")
+	}
 }
 repositories {
-    mavenCentral()
-    maven {
-        name = "Spring Repositories"
-        url = uri("https://repo.spring.io/libs-release/")
-    }
-    gradlePluginPortal()
+	mavenCentral()
+	maven {
+		name = "Spring Repositories"
+		url = uri("https://repo.spring.io/libs-release/")
+	}
+	gradlePluginPortal()
 }
 
 publishing {
-    repositories {
-        if (project.hasProperty("localFileRepo")) {
-            maven {
-                name = "File-Based-Local-Repository"
-                url = uri("file://${property("localFileRepo")}")
-            }
-        }
-    }
+	repositories {
+		maven {
+			// change URLs to point to your repos, e.g. http://my.org/repo
+			val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+			val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+			url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+			credentials {
+				username = System.getenv("OSSRH_USER")
+				password = System.getenv("OSSRH_PASS")
+			}
+		}
+	}
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
-    implementation(group = "khttp", name = "khttp", version = "1.0.0")
-    implementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
-    implementation(group = "org.awaitility", name = "awaitility-kotlin", version = "4.0.2")
-    implementation(files("$projectDir/libs/gradle-processes-0.5.0.jar"))
+	implementation(kotlin("stdlib-jdk8"))
+	implementation(kotlin("reflect"))
+	implementation(group = "khttp", name = "khttp", version = "1.0.0")
+	implementation(group = "com.google.code.gson", name = "gson", version = "2.8.6")
+	implementation(group = "org.awaitility", name = "awaitility-kotlin", version = "4.0.2")
+	implementation(files("$projectDir/libs/gradle-processes-0.5.0.jar"))
 
-    testImplementation(gradleTestKit())
-    testImplementation("junit:junit:4.13")
-    testImplementation("com.beust:klaxon:5.2")
+	testImplementation(gradleTestKit())
+	testImplementation("junit:junit:4.13")
+	testImplementation("com.beust:klaxon:5.2")
 }
 
 gradlePlugin {
-    plugins {
-        create("springdoc-gradle-plugin") {
-            id = "org.springdoc.openapi-gradle-plugin"
-            displayName = "A Gradle plugin for the springdoc-openapi library"
-            description = " This plugin uses springdoc-openapi to generate an OpenAPI description at build time"
-            implementationClass = "org.springdoc.openapi.gradle.plugin.OpenApiGradlePlugin"
-        }
-    }
+	plugins {
+		create("springdoc-gradle-plugin") {
+			id = "org.springdoc.openapi-gradle-plugin"
+			displayName = "A Gradle plugin for the springdoc-openapi library"
+			description = " This plugin uses springdoc-openapi to generate an OpenAPI description at build time"
+			implementationClass = "org.springdoc.openapi.gradle.plugin.OpenApiGradlePlugin"
+		}
+	}
 }
 
 pluginBundle {
-    website = "https://github.com/springdoc/springdoc-openapi-gradle-plugin"
-    vcsUrl = "https://github.com/springdoc/springdoc-openapi-gradle-plugin.git"
-    tags = listOf("springdoc", "openapi", "swagger")
+	website = "https://github.com/springdoc/springdoc-openapi-gradle-plugin"
+	vcsUrl = "https://github.com/springdoc/springdoc-openapi-gradle-plugin.git"
+	tags = listOf("springdoc", "openapi", "swagger")
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
+	compileKotlin {
+		kotlinOptions.jvmTarget = "1.8"
+	}
+	compileTestKotlin {
+		kotlinOptions.jvmTarget = "1.8"
+	}
 }
