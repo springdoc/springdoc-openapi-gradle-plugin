2986e64df7a5e02eef1f3151e399e343381910f4
Jamie McDonald <jamie@qwyck.net>
2021-02-26 10:15:28 +0000

    Add ability to generate multiple OpenAPI docs
    This commit adds a new extension property `groupedApiMappings` allowing
    a user of this plugin to specify multiple mappings of api url to file
    name. This is intended for use in projects utilising the springdoc
    GroupedOpenApi feature.
    
    When the `groupedApiMappings` property is specified the single api
    properties `apiDocsUrl` and `outputFileName` are ignored. The plugin
    attempts to download each mapping in turn.
    
    Fixes #40

--- src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiExtension.kt
+++ src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiExtension.kt
@@ -2,6 +2,7 @@ package org.springdoc.openapi.gradle.plugin
 
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Property
 import javax.inject.Inject
 
@@ -11,4 +12,5 @@ open class OpenApiExtension @Inject constructor(project: Project) {
     val outputDir: DirectoryProperty = project.objects.directoryProperty()
     val waitTimeInSeconds: Property<Int> = project.objects.property(Int::class.java)
     val forkProperties: Property<Any> = project.objects.property(Any::class.java)
+    val groupedApiMappings: MapProperty<String, String> = project.objects.mapProperty(String::class.java, String::class.java)
 }
