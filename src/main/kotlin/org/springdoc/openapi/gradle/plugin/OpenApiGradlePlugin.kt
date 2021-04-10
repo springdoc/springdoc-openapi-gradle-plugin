f15ebd6f986d78f2fd8f073ff303e189ca330a34
Steven Grimm <steven@terraformation.com>
2021-04-09 21:41:40 -0700

    Make plugin work under Gradle 7
    Gradle 7 requires that task properties be annotated to indicate how they should be
    treated when calculating what artifacts need to be rebuilt. One of the methods in
    the gradle-processes plugin was missing an annotation, which caused the plugin to
    bomb out under Gradle 7.
    
    Fix it by subclassing the task in question and annotating the method.

--- src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiGradlePlugin.kt
+++ src/main/kotlin/org/springdoc/openapi/gradle/plugin/OpenApiGradlePlugin.kt
@@ -1,6 +1,5 @@
 package org.springdoc.openapi.gradle.plugin
 
-import com.github.jengelman.gradle.plugins.processes.tasks.Fork
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.Logging
@@ -26,7 +25,7 @@ open class OpenApiGradlePlugin : Plugin<Project> {
 			}
 
 			// Create a forked version spring boot run task
-			val forkedSpringBoot = project.tasks.register(FORKED_SPRING_BOOT_RUN_TASK_NAME, Fork::class.java) { fork ->
+			val forkedSpringBoot = project.tasks.register(FORKED_SPRING_BOOT_RUN_TASK_NAME, AnnotatedFork::class.java) { fork ->
 				fork.dependsOn(bootJarTask)
 
 				fork.onlyIf {
